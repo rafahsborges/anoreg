355c8542fe98153eb79a6b543ff93614ca980828
Rafael Souza Borges <rafael.borges@basis.com.br>
2019-12-10 13:33:47 -0300

    Arquivos Iniciais

--- init.php
+++ init.php
@@ -9,6 +9,4 @@ define('MYSQL_PASS', 'senha');
 define('MYSQL_DBNAME', 'nome_do_banco');
 
 // configurações do PHP
-ini_set('display_errors', true);
-error_reporting(E_ALL);
 date_default_timezone_set('America/Sao_Paulo');
