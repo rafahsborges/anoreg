b5237147c2de0b76f5939fb30768cbf8db2217eb
Rafael Souza Borges <rafael.borges@basis.com.br>
2019-12-11 22:54:45 -0300

    Arquivos Iniciais

--- init.php
+++ init.php
@@ -10,3 +10,25 @@ define('MYSQL_DBNAME', 'nome_do_banco');
 
 // configurações do PHP
 date_default_timezone_set('America/Sao_Paulo');
+
+
+/*
+
+CREATE TABLE IF NOT EXISTS `cartorios` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`nome` VARCHAR(255) NOT NULL,
+	`razao` VARCHAR(255) NOT NULL,
+	`documento` VARCHAR(255) NOT NULL,
+	`cep` INT(8) NOT NULL,
+	`endereco` VARCHAR(255) NOT NULL,
+	`bairro` VARCHAR(255) NOT NULL,
+	`cidade` VARCHAR(255) NOT NULL,
+	`uf` VARCHAR(2) NOT NULL,
+	`telefone` INT(11),
+	`email` VARCHAR(255),
+	`tabeliao` VARCHAR(255),
+	`ativo` BOOLEAN(1),
+	PRIMARY KEY (`id`)
+);
+
+ */
